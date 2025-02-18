01cc34438928c477ab8b8e0ee9daf287afb220bc
pedricolino <cmoris@gmx.com>
2025-02-18 18:00:27 +0100

    add conda to spaceship prompt

--- config/spaceship.zsh
+++ config/spaceship.zsh
@@ -2,10 +2,13 @@ SPACESHIP_PROMPT_ORDER=(
     dir            # Current directory section
     git            # Git section (git_branch + git_status)
     venv           # virtualenv section
+    conda
     exec_time      # Execution time
+    exit_code 
+    time 
+    jobs           # Background jobs indicator
     async          # Async jobs indicator
     line_sep       # Line break
-    jobs           # Background jobs indicator
     char           # Prompt character
 )
 
@@ -14,18 +17,25 @@ _spaceship_add_newline() {
     [ -z "$_should_add_newline" ] && _should_add_newline=true || echo
 }
 precmd_functions+=(_spaceship_add_newline)
+SPACESHIP_TIME_SHOW=true
+SPACESHIP_TIME_COLOR=black
+SPACESHIP_EXIT_CODE_SHOW=true
 
-SPACESHIP_DIR_LOCK_SYMBOL=" 󰌾"
-SPACESHIP_DIR_TRUNC=0
+SPACESHIP_DIR_TRUNC=2
+SPACESHIP_DIR_TRUNC_REPO=true
+SPACESHIP_DIR_TRUNC_PREFIX="…/"
+
+SPACESHIP_CONDA_VERBOSE=true
 
 SPACESHIP_VENV_PREFIX="using "
-SPACESHIP_VENV_SYMBOL=" "
+SPACESHIP_VENV_SYMBOL="> "
 
 SPACESHIP_CHAR_COLOR_SUCCESS="reset"
 if [[ "$TERM" = "linux" ]]; then
     SPACESHIP_CHAR_SYMBOL="> "
 else
-    SPACESHIP_CHAR_SYMBOL="❯ "
+    SPACESHIP_CHAR_SYMBOL="> "
     SPACESHIP_CHAR_SYMBOL_SECONDARY="· "
     SPACESHIP_CHAR_COLOR_SECONDARY=8
 fi
+SPACESHIP_CONDA_SYMBOL=""
